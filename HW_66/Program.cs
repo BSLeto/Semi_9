fa212cf5d6c900e9c45d36f54ebd67cc42ace9d8
Leto <mikhaylichenko2000@bk.ru>
2022-09-07 20:55:07 +0300

    Выполнил задачу 66

--- HW_66/Program.cs
+++ HW_66/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+// Задача 66: Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+int NaturalsDigits(int start, int end)
+{
+    if (start == end) return start;
+
+    return (start + NaturalsDigits(start + 1, end));
+}
+Console.WriteLine(NaturalsDigits(M, N));
